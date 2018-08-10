d192a8cbdab11b2358761fdfee4b99a549aba1af
Luke Schoen <ltfschoen@gmail.com>
2018-08-10 17:15:57 +0200

    Example test setup

--- packages/app-accounts/src/DownloadButton.spec.js
+++ packages/app-accounts/src/DownloadButton.spec.js
@@ -3,19 +3,57 @@
 // of the ISC license. See the LICENSE file for details.
 
 import React from 'react';
+import createReactClass from 'create-react-class';
 import expect from 'expect';
+import sinon from 'sinon';
 import { shallow } from '../test/enzyme';
+// Note: react-test-renderer (i.e. react-test-renderer/shallow) replaces deprecated react-addons-test-utils
+// Note: https://reactjs.org/docs/shallow-renderer.html
+import ShallowRenderer from 'react-test-renderer/shallow'; // ES6
+const proxyquire = require('proxyquire').noCallThru();
 
-import DownloadButton from './DownloadButton';
+// import DownloadButton from './DownloadButton';
+
+const createStubComponent = () => {
+  return createReactClass({
+      render: () => {
+          return React.DOM.div();
+      }
+  });
+}
+
+const translateStub = createStubComponent();
+
+const sandbox = sinon.sandbox.create();
+
+const DownloadButton = proxyquire('./DownloadButton', {
+    'react': React,
+    './translate': translateStub
+});
 
 describe ('DownloadButton', () => {
   let wrapper;
   
   beforeEach(() => {
+    downloadButtonClass = React.DOM.findDOMNode(DownloadButton).getAttribute('class');
+
     wrapper = shallow(<DownloadButton className='test' />, {});
   });
 
   it('creates the element', () => {
     expect(wrapper).toBeDefined();
   });
+
+  // it('sets password modal open when click download button', () => {
+  //   // Mock State
+  //   const state = {
+  //     isPasswordModalOpen: false
+  //   };
+
+  //   const wrapper = mount(<DownloadButton {...state}/>);
+  //   const downloadDiv = wrapper.find('accounts--Address-download');
+  //   const downloadButton = downloadDiv.find('button').last();
+  //   downloadButton.simulate('click');
+  //   expect(wrapper.state().isPasswordModalOpen).toBe(true);
+  // });
 });
--- packages/app-accounts/test/enzyme.js
+++ packages/app-accounts/test/enzyme.js
@@ -2,11 +2,31 @@
 // This software may be modified and distributed under the terms
 // of the ISC license. See the LICENSE file for details.
 
+// Enzyme
 const Adapter = require('enzyme-adapter-react-16');
 const Enzyme = require('enzyme');
 
 Enzyme.configure({
   adapter: new Adapter()
 });
+
+// global.shallow = Enzyme.shallow;
+// global.mount = Enzyme.mount;
+// global.render = Enzyme.render;
+
+// // Enzyme Helpers
+// global.itRenders = Component => {
+//   const wrapper = Enzyme.shallow(Component);
+//   expect(Enzyme.shallow(Component).length).to.eql(1);
+//   return wrapper;
+// };
+
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  clear: jest.fn()
+}
+
+global.localStorage = localStorageMock
 
 module.exports = Enzyme;
