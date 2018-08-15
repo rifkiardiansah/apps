12242fb699887e9ee907bd1d1859df221ded5d83
Luke Schoen <ltfschoen@gmail.com>
2018-08-15 16:24:37 +0200

    WIP

--- packages/app-accounts/src/DownloadModal.tsx
+++ packages/app-accounts/src/DownloadModal.tsx
@@ -2,20 +2,31 @@
 // This software may be modified and distributed under the terms
 // of the ISC license. See the LICENSE file for details.
 
+import { ApiProps } from '@polkadot/ui-react-rx/types';
 import { I18nProps, BareProps } from '@polkadot/ui-app/types';
 
 import React from 'react';
 import { Trans } from 'react-i18next';
 import AddressMini from '@polkadot/ui-app/AddressMini';
+import AddressSummary from '@polkadot/ui-app/AddressSummary';
 import Button from '@polkadot/ui-app/Button';
 import Modal from '@polkadot/ui-app/Modal';
 import classes from '@polkadot/ui-app/util/classes';
 import keyring from '@polkadot/ui-keyring/index';
 import Unlock from '@polkadot/ui-signer/Unlock';
+import withApi from '@polkadot/ui-react-rx/with/api';
 
 import translate from './translate';
 
-type Props = I18nProps & BareProps & {
+// const WithApiDebug = withApi(
+//   (apiProps) => (
+//     <pre style={{ background: '#f5f5f5', padding: '0.5em' }}>
+//       {JSON.stringify(apiProps, undefined, 2)}
+//     </pre>
+//   )
+// );
+
+type Props = I18nProps & ApiProps & BareProps & {
   address: string,
   error?: React.ReactNode,
   handleDownloadAccount: () => void,
@@ -27,6 +38,10 @@ type Props = I18nProps & BareProps & {
 };
 
 export class DownloadModal extends React.PureComponent<Props> {
+  constructor (props: Props) {
+    super(props);
+  }
+
   render () {
     const { address, className, hidePasswordModal, isPasswordModalOpen, style } = this.props;
 
@@ -39,9 +54,11 @@ export class DownloadModal extends React.PureComponent<Props> {
         size='mini'
         style={style}
       >
+        {/* <WithApiDebug /> */}
         <Modal.Content>
           <div className='ui--grid'>
             <div className='accounts--Address-modal'>
+              <AddressSummary value={address} />
               <AddressMini
                 isShort
                 value={address}
@@ -110,4 +127,18 @@ export class DownloadModal extends React.PureComponent<Props> {
   }
 }
 
-export default translate(DownloadModal);
+const Component: React.ComponentType<any> = translate(
+  // withApi(DownloadModal)
+  withApi<{}, Props>(DownloadModal)
+);
+
+//   (apiProps) => (
+//     <pre style={{ background: '#f5f5f5', padding: '0.5em' }}>
+//       {JSON.stringify(apiProps, undefined, 2)}
+//     </pre>
+//   )
+// );
+
+export default Component;
+
+// export default translate(DownloadModal);
